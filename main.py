2aa3b55af3183714b162edc5e1a2df71087990e0
Ahmad Alwareh <ahmadalwareh@outlook.com>
2021-12-06 16:47:12 +0200

    created demo file

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 import docx
 from docx import Document
+from docx.shared import Inches
 
 doc = Document()
 
 
+doc.add_heading('Document Title', 0)
+
+p = doc.add_paragraph('A plain paragraph having some ')
+p.add_run('bold').bold = True
+p.add_run(' and some ')
+p.add_run('italic.').italic = True
+
+records = (
+    (3, '101', 'Spam'),
+    (7, '422', 'Eggs'),
+    (4, '631', 'Spam, spam, eggs, and spam')
+)
+
+table = doc.add_table(rows=1, cols=3)
+hdr_cells = table.rows[0].cells
+hdr_cells[0].text = 'Qty'
+hdr_cells[1].text = 'Id'
+hdr_cells[2].text = 'Desc'
+for qty, id, desc in records:
+    row_cells = table.add_row().cells
+    row_cells[0].text = str(qty)
+    row_cells[1].text = id
+    row_cells[2].text = desc
+
+doc.add_page_break()
+
+doc.save('demo.docx')
