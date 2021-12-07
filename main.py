32e809b089dc4c2ba11008ee33189b06d49aee09
Ahmad Alwareh <ahmadalwareh@outlook.com>
2021-12-07 10:02:43 +0200

    edited italic

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ doc.add_heading('Document Title', 0)
 p = doc.add_paragraph('A plain paragraph having some ')
 p.add_run('bold').bold = True
 p.add_run(' and some ')
-p.add_run('italic.').italic = True
+p.add_run('italic').italic = True
 
 records = (
     (3, '101', 'Spam'),
